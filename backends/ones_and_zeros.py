d2a605bbdec0a6a732543f5dbf13788a60566a0f
iTrooz <hey@itrooz.fr>
2023-03-01 18:56:01 +0100

    ones and zeros backend

--- backends/ones_and_zeros.py
+++ backends/ones_and_zeros.py
@@ -1,5 +1,3 @@
-import time
-
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -12,7 +10,15 @@ class OnesAndZeros:
         self.tableIndexes = tableIndexes
 
     def run(self):
-        for i,j in self.tableIndexes:
+        step = self.device.get_size()/len(self.tableIndexes)
+        for (x, y), i in zip(self.tableIndexes, range(len(self.tableIndexes))):
             item = QTableWidgetItem()
-            item.setBackground(QBrush(Qt.red))
-            self.table.setItem(i, j, item)
+            self.table.setItem(x, y, item)
+
+            byte = self.device.get_bytes(int(step*i), 1)[0]
+            ones_count = format(byte, 'b').count('1')            
+            color_comp = int(ones_count*256/8)
+
+            item.setBackground(QBrush(QColor(color_comp, color_comp, color_comp)))
+
+
