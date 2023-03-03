696c3d2baef2c0dbf7f4cf8781bedbddb5fb8c34
iTrooz <hey@itrooz.fr>
2023-03-03 15:57:35 +0100

    delete ones_and_zeros_avg backend

--- backends/ones_and_zeros_avg.py
+++
@@ -1,9 +0,0 @@
-class OnesAndZerosAvg:
-    
-    def __init__(self, table, device, tableIndexes):
-        self.table = table
-        self.device = device
-        self.tableIndexes = tableIndexes
-
-    def run(self):
-        pass
